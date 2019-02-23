8202fb560835b17655ebe84a9e1bc2237e39dc5e
huangqijun <huangqijun@huangqijundeMacBook-Pro-2.local>
2019-02-23 18:10:30 +0800

    @SpringCloudApplication

--- com.study.cloud.userserver/src/main/java/com/study/cloud/userserver/UserserverApplication.java
+++ com.study.cloud.userserver/src/main/java/com/study/cloud/userserver/UserserverApplication.java
@@ -2,10 +2,11 @@ package com.study.cloud.userserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringCloudApplication
 @EnableEurekaClient
 @EnableFeignClients
 public class UserserverApplication {
--- com.study.cloud.userserver/src/main/java/com/study/cloud/userserver/controller/UserController.java
+++ com.study.cloud.userserver/src/main/java/com/study/cloud/userserver/controller/UserController.java
@@ -4,6 +4,7 @@ import com.study.cloud.userserver.server.feign.FeignUserServiceImpl;
 import com.study.cloud.userserver.server.smodel.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
